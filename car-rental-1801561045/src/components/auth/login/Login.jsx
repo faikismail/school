a18f3b9eeb090fae13ed78f455e327a6c551489a
faikismail <85372885+faikismail@users.noreply.github.com>
2021-06-05 01:03:00 +0300

    Add files via upload

---
+++ car-rental-1801561045/src/components/auth/login/Login.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { login } from "../../../core/services/AuthService";
+import './Login.css'; 
+import { Redirect, Link } from 'react-router-dom';
+
+export function Login(props) {
+    const [userData, setUserData] = useState(null);
+    const [redirect, setRedirect] = useState(false);
+    const [error, setError] = useState('');
+
+    const onInputChange = (event) => {
+        event.persist();
+
+        setUserData((prevState) => ({
+            ...prevState,
+            [event.target.name]: event.target.value
+        }));
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        login(userData).then(_ => {
+            console.log('success!');
+            setRedirect(true);
+        })
+        .catch(err => setError(err.message));
+    }
+
+    return (
+        <>
+        { redirect && <Redirect to='/' />}
+        <div className="login-form-wrapper">
+            <form className="login-form" onSubmit={onFormSubmit}>   
+            { error && <span className="text-danger">{error}</span> }
+                <div className="form-group"> 
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" id="email" name="email" className="form-control" onChange={onInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" id="password" name="password" className="form-control" onChange={onInputChange} required />
+                </div>
+                <button className="btn btn-primary">Login</button>
+                <div>
+                    <Link to="/register">Don't have an account yet?</Link>
+                </div>
+            </form>
+        </div>
+        </> 
+    );
+}
---
+++ car-rental-1801561045/src/components/layout/footer/Footer.jsx
@@ -0,0 +1,16 @@
+const styles = {
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    backgroundColor: 'blue'
+};
+
+
+export function Footer() {
+    return(
+        <footer className="footer" style={styles}>
+           .
+        
+        </footer>
+    )
+}   
